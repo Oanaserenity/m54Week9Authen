762195069a6ec0420c66715c9e372bf6ed7e2e37
Oana <oanaloredana78@yahoo.co.uk>
2024-03-08 16:01:47 +0000

    pass post

---
+++ src/users/controllers.js
@@ -0,0 +1,20 @@
+const User = require("./model");
+
+const signupUser = async (req, res) => {
+    try {
+        const user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        res.status(201).json({ message: "user added", user: user});
+    } catch (error) {
+        res.status(500).json({ message: error.message, error: error });
+    }
+    };
+
+    module.exports = {
+        signupUser: signupUser,
+    };
+    
